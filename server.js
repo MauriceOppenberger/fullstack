66e2ba58d1e46b6e4e04f8a1c809875b8cac84dd
Maurice Oppenberger <mauriceoppenberger@gmail.com>
2020-03-22 14:53:16 -0400

    deploy

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use("/admin", adminRoute);
 if (process.env.Node_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get("*", function(req, res) {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
