a68e9679f3c7ded18acb43d7301fc7a4478237f4
Maurice Oppenberger <mauriceoppenberger@gmail.com>
2020-03-22 23:09:12 -0400

    updeate and deploy

--- server.js
+++ server.js
@@ -16,14 +16,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use((req, res, next) => {
-  // res.header(
-  //   "Access-Control-Allow-Methods",
-  //   "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-  // );
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  // );
   res.header("Access-Control-Allow-Credentials", true);
 
   next();
